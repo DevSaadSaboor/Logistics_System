84995e3fd7eb3d5143dbcfe9215c10213f688136
DevSaadSaboor <saadsaboor755@gmail.com>
2026-05-11 18:41:15 +0500

    fix render migration database url

--- migrations/env.py
+++ migrations/env.py
@@ -16,9 +16,11 @@ config = context.config
 
 # Override sqlalchemy.url from environment variable if set.
 # SYNC_DATABASE_URL uses psycopg2 (synchronous) which is required by Alembic.
-db_url = os.environ.get("SYNC_DATABASE_URL")
-if db_url:
-    config.set_main_option("sqlalchemy.url", db_url)
+db_url = os.environ["SYNC_DATABASE_URL"]
+config.set_main_option(
+    "sqlalchemy.url",
+    db_url
+)
 
 # Interpret the config file for Python logging.
 if config.config_file_name is not None:
