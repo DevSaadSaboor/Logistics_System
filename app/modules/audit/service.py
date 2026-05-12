f8b3f0c16664cd96adf48003224a97b4fc84ed1e
DevSaadSaboor <saadsaboor755@gmail.com>
2026-05-12 17:43:19 +0500

    Fixed Shipment create

--- app/modules/audit/service.py
+++ app/modules/audit/service.py
@@ -7,7 +7,7 @@ class AuditService:
         self.db = db
 
     async def log(self,action:str,resources_type:str,resource_id:str | None = None,user_id:str | None = None,
-            tenant_id:str | None = None,ip_address:str | None = None,user_agent:str | None = None,metadate_json:dict | None = None):
+            tenant_id:str | None = None,ip_address:str | None = None,user_agent:str | None = None,metadata_json:dict | None = None):
         
         audig_log = AuditLog (
             action = action,
@@ -17,7 +17,7 @@ class AuditService:
             tenant_id = tenant_id,
             ip_address = ip_address,
             user_agent = user_agent,
-            metadate_json = metadate_json
+            metadata_json = metadata_json
         )
 
         self.db.add(audig_log)
